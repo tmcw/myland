20ccef92daaa99b3ce8a42a50d31cc8195398741
Tom MacWright <tom@macwright.org>
2014-10-02 08:56:18 -0400

    Add timedownload

---
+++ timedownload.js
@@ -0,0 +1,13 @@
+var fs = require('fs'),
+  http = require('http');
+
+var URL = 'http://ie.trafficland.com/200013/full?system=ddot&pubtoken=defd73dd750050992f058c40d231efdabe7&t=1412254287091';
+
+setInterval(snap, 1000 * 5);
+
+function snap() {
+  http
+    .get(URL, function(resp) {
+      resp.pipe(fs.createWriteStream('./captures/' + Date.now() + '.jpg'));
+    });
+}
